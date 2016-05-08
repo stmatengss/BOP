6a0962cde7eca54b9e88931339c28d0aa0bf85ce
stmatengss <805522925@qq.com>
2016-05-08 23:32:35 +0800

    Added files via upload

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import codecs
 import tools
 
 urls = (
-        '?id1=(\d+)&id2=(\d+)', 'Result'
+        '/id1=(\d+)&id2=(\d+)', 'Result'
 )
 
 class Result:
         def GET(self, id1, id2):
+            print "!!!!"    
             return tools.dealIdToId(id1, id2)
 
 app = web.application(urls, globals())
 application = app.wsgifunc()
 
-if __name__ == "__main__"
+if __name__ == "__main__":
         app.run()
 
 
--- tools.py
+++ tools.py
@@ -19,19 +19,31 @@ def toExpr(id, attributes):
         return 'expr=Id=' + id + '&count=10000&attributes=' + attributes
         
 def idOrAuId(id):
-        ob = getJson('expr=Composite(AA.AuId='+id+')&attributes=Id')
-        if b['entities']:
+        ob = getJson('expr=Composite(AA.AuId=' + id + ')&attributes=Id')
+        if b.has_key('entities'):
                 return True
         else:
                 return False
 
+def classify(id1, id2):
+        st1 = idOrAuId(id1)
+        st2 = idOrAuId(id2)
+        if st1 and st2:
+                return dealIdToId(id1, id2)
+        elif st1:
+                return dealIdToAuId(id1, id2)
+        elif st2:
+                return dealAuIdToId(id1, id2)
+        else:
+                return dealAuIdToAuId(id1, id2)
+
 def dealIdToAuId(id1, id2):
         pass
 
-def dealAuIdtoId(id1, id2):
+def dealAuIdToId(id1, id2):
 	pass
 
-def dealAuIdtoAuId(id1, id2):
+def dealAuIdToAuId(id1, id2):
 	pass
 
 def dealIdToId(id1, id2):
@@ -40,30 +52,45 @@ def dealIdToId(id1, id2):
         json2 = getJson(toExpr(str(id2), allAttr))
         Set1 = []
         Set2 = []
-        entity1 = json1['entities']
-        entity2 = json1['entities']
-        AuId1 = [i['AuId'] for i in entity1['AA'] ]
-        AuId2 = [i['AuId'] for i in entity2['AA'] ]
-        Set1.append(AuId1)
-        Set2.append(AuId2)
-        FId1 = [i['Fid'] for i in entity1['FId']]
-        FId2 = [i['Fid'] for i in entity2['FId']]
-        Set1.append(FId1)
-        Set2.append(FId2)
-        CId1 = [i['Cid'] for i in entity1['CId']]
-        CId2 = [i['Cid'] for i in entity2['CId']]
-        Set1.append(CId1)
-        Set2.append(CId2)
-        JId1 = [i['Jid'] for i in entity1['JId']]
-        JId2 = [i['Jid'] for i in entity2['JId']]
-        Set1.append(JId1)
-        Set2.append(JId2)
+        entity1 = json1['entities'][0]
+        entity2 = json1['entities'][0]
+        if entity1.has_key('AA'):
+                AuId1 = [i['AuId'] for i in entity1['AA']]
+                Set1.append(AuId1)
+        if entity2.has_key('AA'):
+                AuId2 = [i['AuId'] for i in entity2['AA']]
+                Set2.append(AuId2)
+        
+        if entity1.has_key('F'):
+                FId1 = [i['FId'] for i in entity1['F']]
+                Set1.append(FId1)
+        if entity2.has_key('F'):
+                FId2 = [i['FId'] for i in entity2['F']]
+                Set2.append(FId2)
+        
+        if entity1.has_key('C'):
+                CId1 = [i['CId'] for i in entity1['C']]
+                Set1.append(CId1)
+        if entity2.has_key('C'):
+                CId2 = [i['CId'] for i in entity2['C']]
+                Set2.append(CId2)
+        
+        if entity1.has_key('J'):
+                JId1 = [entity1['J']['JId']]
+                Set1.append(JId1)
+        if entity2.has_key('J'):
+                JId2 = [entity2['J']['JId']]
+                Set2.append(JId2)
+
+        intId1 = int(id1)
+        intId2 = int(id2)
+        
         intersection = []
         for i,j in zip(Set1,Set2):
 		intersection = intersection + i and j
-	res = [[id1,id2]]
+	res = [[intId1,intId2]]
 	for i in intersection:
-		res.append([id1,i,id2])
+		res.append([intId1,i,intId2])
 	return json.dumps(res)
         
         
